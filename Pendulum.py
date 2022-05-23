2a92226037fda1b9cf72ac3c40abc5d9d78e5164
Christopher Tan <72955765+Christopher-Tan@users.noreply.github.com>
2022-05-22 23:31:02 -0400

    Calculated forces on each individual mass correctly based on tension in ropes

--- Pendulum.py
+++ Pendulum.py
@@ -10,9 +10,10 @@ class Pendulum:
         self.end.n = length, new_node
         self.end = new_node
         self.start.max_force()
+        self.start.force()
         
 class Node:
-    #update values
+    #Update values
     def coords(self):
         if (self.p[1] != None):
             self.p[1].coords()
@@ -22,13 +23,19 @@ class Node:
         if (self.n[1] != None):
             self.n[1].max_force()
             self.f_max = self.n[1].f_max + self.m * g
+    def force(self):
+        if (self.n[1] != None):
+            self.n[1].force()
+            self.f = [self.m * g * math.sin(self.x-math.pi) + self.n[1].f[0] * math.cos(self.n[1].x-self.x), self.m * g * math.cos(self.x-math.pi) + self.n[1].f[0] * math.sin(self.n[1].x-self.x)]
+        else:
+            self.f = [self.m * g * math.sin(self.x-math.pi), self.m * g * math.cos(self.x-math.pi)]
+
     
     def __init__(self, mass, length, previous = None):
         self.m = mass
         #Angular
         self.x = 0
         self.v = 0
-        self.a = 0
         self.f_max = mass * g
         #Link
         self.p = [length, previous]
@@ -36,4 +43,7 @@ class Node:
         #Cartesian
         self.c = [0,0]
         self.coords()
+        #Forces
+        self.f = [0,0] #Normal, Tangential
+
 a = Pendulum()
