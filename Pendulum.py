f78484ca7ac6b77f4eb1fbd47ad09a891f682787
Christopher Tan <72955765+Christopher-Tan@users.noreply.github.com>
2022-06-04 21:41:00 -0400

    Forgot to push recent updates for the last week. Another attempt at trying to fix the physics, certain things are working better and others worse. I also made multipendulum support and actual animation

--- Pendulum.py
+++ Pendulum.py
@@ -1,12 +1,9 @@
 import math
 import time
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import random
 
-def sum(v1,v2):
-    x = v1[0] * math.cos(v1[1]) + v2[0] * math.cos(v2[1])
-    y = v1[0] * math.sin(v1[1]) + v2[0] * math.sin(v2[1])
-    return [math.sqrt(x ** 2 + y ** 2), math.atan2(y, x)] #magnitude, angle
 #Constants
 g = 10
 class Pendulum:
@@ -20,9 +17,11 @@ class Pendulum:
         self.start.max_force()
         self.start.force()
     def elapse(self, dt):
-        self.start.force()
-        self.end.acceleration()
-        self.start.elapse(dt)
+        s = self.end
+        while (s != None):
+            self.start.force()
+            s.elapse(dt)
+            s = s.p[1]
         self.end.coords()
     def coords(self):
         i = self.start
@@ -31,7 +30,14 @@ class Pendulum:
             c.append(i.c)
             i = i.n[1]
         return c
-        
+    def energy(self):
+      e = 0
+      s = self.start
+      while (s != None):
+        e += 0.5 * s.m * (s.v ** 2) + s.m * g * s.c[1]
+        s = s.n[1]
+      return e
+       
 class Node:
     #Update values
     def coords(self):
@@ -49,61 +55,53 @@ class Node:
             self.f = [self.m * g * math.sin(self.x-math.pi) + self.n[1].f[0] * math.cos(self.n[1].x-self.x), self.m * g * math.cos(self.x-math.pi) + self.n[1].f[0] * math.sin(self.n[1].x-self.x)]
         else:
             self.f = [self.m * g * math.sin(self.x-math.pi), self.m * g * math.cos(self.x-math.pi)]
-    def acceleration(self):
-        if (self.p[1] != None):
-            self.p[1].acceleration()
-            try:
-                self.a = sum(self.p[1].a,[self.f[1]/self.m * math.sin(self.n[1].x-self.x), self.n[1].x])
-            except:
-                self.a = [0,0]
-        else:
-            self.a = [0,0]
     def elapse(self, dt):
-        if (self.p[1] == None):
-            a = 0
-        elif (self.p[1].m == 0):
-            a = (self.f[1] / self.m) / self.p[0]
-        else:
-            a = (self.f[1] / self.m) / (self.p[0] * (1 - (1 / ((self.p[1].m/self.m) + 1))))
-        b = a
         try:
-            a += self.p[1].a[0] * math.cos(self.p[1].a[1] - self.x - 90) / (self.p[0] / ((self.p[1].m/self.m) + 1))
+            a1 = self.f[1] / self.m / self.p[0]
+            self.v += a1 * dt
+            self.x += self.v * dt
         except:
-            a = b
-        self.v += a * dt
-        self.x += self.v * dt
-        if (self.n[1] != None):
-            self.n[1].elapse(dt)
-    
+            pass
+        try:
+            a2 = self.f[1] * math.cos(self.n[1].x - self.x) / self.m / self.n[0]
+            self.n[1].v -= a2 * dt
+            self.n[1].x += self.n[1].v * dt
+        except:
+            pass
+
+   
     def __init__(self, mass, length, previous = None):
         self.m = mass
         #Angular
-        self.x = random.random()
+        self.x = (random.random() - 0.5)/10
         self.v = 0
         self.f_max = mass * g
         #Link
         self.p = [length, previous]
         self.n = [0, None]
         #Cartesian
-        self.a = [0,0] #Not relative
         self.c = [0,0]
         self.coords()
         #Forces
         self.f = [0,0] #Normal, Tangential
+a = [Pendulum() for i in range(1)]
 
-a = Pendulum()
-a.add_node(1,1)
-a.add_node(1,1)
-a.add_node(1,1)
-#a.add_node(1,1)
-while (True):
-    a.elapse(0.01)
-    print(a.start.n[1].a)
+for i in a:
+    i.add_node(1,1)
+    i.add_node(1,1)
+    i.add_node(1,1)
 
-    x = [i[0] for i in a.coords()]
-    y = [i[1] for i in a.coords()]
-    plt.subplots()[0].canvas.draw()
-    plt.xlim(-5,5)
-    plt.ylim(-5,5)
-    plt.plot(x, y, marker="o", markersize=20)
-    plt.show()
+fig = plt.figure()
+
+def animate(i):
+    plt.cla()
+    for i in a:
+        i.elapse(0.01)
+        x = [j[0] for j in i.coords()]
+        y = [j[1] for j in i.coords()]
+        plt.xlim(-10,10)
+        plt.ylim(-10,10)
+        plt.plot(x, y, marker="o", markersize=2)
+
+ani = animation.FuncAnimation(fig, animate, interval=10)
+plt.show()
