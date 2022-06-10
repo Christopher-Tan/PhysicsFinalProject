be6f27209026800d766dbda71b8b835ab8d27bfd
Christopher Tan <72955765+Christopher-Tan@users.noreply.github.com>
2022-06-09 22:28:01 -0400

    Fixed previous instability error, turns out I used a copy operation instead of a deepcopy operation. It seems that the energy is actually conserved now

--- Pendulum.py
+++ Pendulum.py
@@ -3,6 +3,7 @@ import time
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random
+import copy
 
 #Constants
 g = 10
@@ -17,7 +18,7 @@ def f(array):
         w1 = array[0][3]
         w2 = array[1][3]
         return [w1, w2, (-g * (2 * m1 + m2) * math.sin(t1) - m2 * g * math.sin(t1 - 2 * t2) - 2 * math.sin(t1 - t2) * m2 * ((w2 ** 2) * l2 + (w1 ** 2) * l1 * math.cos(t1 - t2))) / (l1 * (2 * m1 + m2 - m2 * math.cos(2 * t1 - 2 * t2))), (2 * math.sin(t1 - t2) * ((w1 ** 2) * l1 * (m1 + m2) + g * (m1 + m2) * math.cos(t1) + (w2 ** 2) * l2 * m2 * math.cos(t1 - t2))) / (l2 * (2 * m1 + m2 - m2 * math.cos(2 * t1 - 2 * t2)))]
-        
+
 
 class Pendulum():
     def __init__(s):
@@ -27,21 +28,21 @@ class Pendulum():
     def remove_node(s):
         s.nodes.pop()
     def elapse(s, dt):
-        ms = s.nodes.copy()
+        ms = copy.deepcopy(s.nodes)
         a = f(ms)
-        ms = s.nodes.copy()
+        ms = copy.deepcopy(s.nodes)
         ms[0][2] += a[0] * (dt / 2)
         ms[1][2] += a[1] * (dt / 2)
         ms[0][3] += a[2] * (dt / 2)
         ms[1][3] += a[3] * (dt / 2)
         b = f(ms)
-        ms = s.nodes.copy()
+        ms = copy.deepcopy(s.nodes)
         ms[0][2] += b[0] * (dt / 2)
         ms[1][2] += b[1] * (dt / 2)
         ms[0][3] += b[2] * (dt / 2)
         ms[1][3] += b[3] * (dt / 2)
         c = f(ms)
-        ms = s.nodes.copy()
+        ms = copy.deepcopy(s.nodes)
         ms[0][2] += c[0] * dt
         ms[1][2] += c[1] * dt
         ms[0][3] += c[2] * dt
@@ -72,7 +73,7 @@ fig = plt.figure()
 def animate(i):
     plt.cla()
     for i in p:
-        i.elapse(0.01)
+        i.elapse(0.05)
         x = [j[0] for j in i.coords()]
         y = [j[1] for j in i.coords()]
         plt.xlim(-10,10)
