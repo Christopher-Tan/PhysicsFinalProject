efd211a158b3e501335000d5ec1858221eedceaa
Christopher Tan <72955765+Christopher-Tan@users.noreply.github.com>
2022-06-12 16:01:07 -0400

    Added mass calculation and reverted to newtonian method again

--- Pendulum.py
+++ Pendulum.py
@@ -17,15 +17,16 @@ while True:
             m2 = array[1][0]
             l1 = array[0][1]
             l2 = array[1][1]
-            t1 = array[0][2]
+            t1 = array[0][2] 
             t2 = array[1][2]
             w1 = array[0][3]
             w2 = array[1][3]
+            
             f1 = (-l2 * (m2) * (w2 ** 2) * math.sin(t1 - t2) - g * math.sin(t1)) / l1
             f2 = (l1 * (w1 ** 2) * math.sin(t1 - t2) - g * math.sin(t2)) / l2
             a1 = l2 * m2 * math.cos(t1 - t2) / (l1 * (m1 + m2))
             a2 = l1 * math.cos(t1 - t2) / l2
-            return [w1, w2, (f1 - a1 * f2) / (1 - a1 * a2), (f2 - a2 * f1) / (1 - a1 * a2)]
+            return [w1, w2, (-g * (2 * m1 + m2) * math.sin(t1) - m2 * g * math.sin(t1 - 2 * t2) - 2 * math.sin(t1 - t2) * m2 * ((w2 ** 2) * l2 + (w1 ** 2) * l1 * math.cos(t1 - t2))) / (l1 * (2 * m1 + m2 - m2 * math.cos(2 * t1 - 2 * t2))), (2 * math.sin(t1 - t2) * ((w1 ** 2) * l1 * (m1 + m2) + g * (m1 + m2) * math.cos(t1) + (w2 ** 2) * l2 * m2 * math.cos(t1 - t2))) / (l2 * (2 * m1 + m2 - m2 * math.cos(2 * t1 - 2 * t2)))]
         else:
             M = []
             n = len(array)
@@ -128,7 +129,7 @@ while True:
     cid1 = fig.canvas.mpl_connect('button_press_event', onclick)
     cid2 = fig.canvas.mpl_connect('motion_notify_event', onmove)
     def flips(event):
-        global s,p,h,counter,spendulum,snode,bhelp,end
+        global s,p,h,counter,spendulum,snode,bhelp,end,smass,smassax
         s = not s
         h = False
         c = None
@@ -136,6 +137,8 @@ while True:
             for i in sp.nodes:
                 i[1] = 1
         if s:
+            if snode.val == 2:
+                sp.nodes[1][0] = round((2 ** smass.val) * 1000) / 1000
             p = [copy.deepcopy(sp) for i in range(spendulum.val)]
             for i in p:
                 for j in i.nodes:
@@ -145,6 +148,7 @@ while True:
             fig.delaxes(ax[2])
             fig.delaxes(ax[3])
             fig.delaxes(ax[4])
+            fig.delaxes(smassax)
             fig.canvas.mpl_disconnect(cid1)
             fig.canvas.mpl_disconnect(cid2)
             ax[0].set_axis_on()
@@ -166,10 +170,23 @@ while True:
 
     spendulum = Slider(ax[2], "Pendulum(s)", 1, 100, 50, valstep=1, clip_on=False)
     snode = Slider(ax[4], "Node(s)", 1, 10, 2, valstep=1, clip_on=False)
+    smass = None
     def animate(i):
-        global spendulum, p
+        global spendulum, p, smassax, smass
         if s == False:
             ax[0].cla()
+            if snode.val == 2 and smass == None:
+                smassax = plt.axes([0.01, 0.1, 0.02, 0.8])
+                smassax.set_axis_off()
+                smassax.xaxis.set_visible(False)
+                smassax.yaxis.set_visible(False)
+                smass = Slider(smassax, "M2:M1", -3, 3, 0, clip_on=False, orientation='vertical')
+            elif snode.val == 2 and smass != None:
+                smass.valtext.set_text(round((2 ** smass.val) * 1000) / 1000)
+            elif smass != None:
+                fig.delaxes(smassax)
+                smass = None
+            
             if (snode.val < len(sp.nodes)):
                 sp.remove_node()
             elif (snode.val > len(sp.nodes)):
